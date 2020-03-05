7651cd3ab24ff48e83438c8c8158a17c6bbcf7f2
Georg Jaehnig <georg@jaehnig.org>
2020-03-05 22:34:32 +0100

    Cleanup.

--- link-audit.sh
+++ link-audit.sh
@@ -1,16 +1,34 @@
+#!/bin/bash
 if [ $# -lt 1 ]; then
+  echo 'Usage:'
   echo $0 url
   exit 1
 fi
 
-rm -r /tmp/link-audit 2> /dev/null
+if [[ -z ${LINKAUDIT_RECIPIENT} ]];
+then
+  echo '$LINKAUDIT_RECIPIENT is not set.'
+  echo 'You can set it with:'
+  echo 'export LINKAUDIT_RECIPIENT=foo@example.com'
+fi
 
+rm -r /tmp/link-audit 2> /dev/null
+ 
 LINKAUDIT_DIR=/tmp/link-audit
 mkdir -p $LINKAUDIT_DIR
 
-cd $LINKAUDIT_DIR
-wget -r -l1 --span-hosts -e robots=off --no-check-certificate "$1" 2> $LINKAUDIT_DIR/log.txt
+wget \
+  -r \
+  -l2 \
+  -e robots=off \
+  --no-check-certificate \
+  --directory-prefix=$LINKAUDIT_DIR \
+  "$1" 2> $LINKAUDIT_DIR/log.txt
 
-grep --before-context=3 -E '(response... (4|5)|unable to resolve|no certificate)' $LINKAUDIT_DIR/log.txt
-
-cd - > /dev/null
+{ \
+  echo "Subject: Link Audit of $1"; \
+  grep \
+    --before-context=3 \
+    -E '(response... (4|5)|unable to resolve|no certificate)' \
+    $LINKAUDIT_DIR/log.txt ; \
+} | /usr/sbin/sendmail $LINKAUDIT_RECIPIENT
